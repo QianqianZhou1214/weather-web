7e3368226015cd7533b1d03a9c4aa2c5a8df575e
Qianqian Zhou <qianqian.zhou@stud.th-rosenheim.de>
2024-05-24 13:09:24 +0200

    SmallCity class created

--- psta-src/src/main/java/th/rosenheim/oop/SmallCity.java
+++ psta-src/src/main/java/th/rosenheim/oop/SmallCity.java
@@ -3,16 +3,22 @@ package th.rosenheim.oop;
 public class SmallCity extends City {
 
     public SmallCity(String cityName, int weatherCondition) {
+
         super(cityName, weatherCondition);
+
     }
 
     @Override
     public String getContent() {
-        return "";
+
+        return "<p>It is " + weatherCondition + " in " + specialName(cityName) + ".</p>";
+
     }
 
     @Override
     public String getURL() {
-        return "";
+
+        return "weather_small_city_" + specialName(cityName) + ".html";
+
     }
 }
