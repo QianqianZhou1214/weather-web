46fa814d21d54838f50e53d8fa93622de1ade5d1
Qianqian Zhou <qianqian.zhou@stud.th-rosenheim.de>
2024-05-24 12:25:28 +0200

    create structure of subclasses

--- psta-src/src/main/java/th/rosenheim/oop/BigCity.java
+++ psta-src/src/main/java/th/rosenheim/oop/BigCity.java
@@ -1,4 +1,17 @@
 package th.rosenheim.oop;
 
 public class BigCity extends City {
+    public BigCity(String cityName, int weatherCondition) {
+        super(cityName, weatherCondition);
+    }
+
+    @Override
+    public String getContent() {
+        return "";
+    }
+
+    @Override
+    public String getURL() {
+        return "";
+    }
 }
--- psta-src/src/main/java/th/rosenheim/oop/SmallCity.java
+++ psta-src/src/main/java/th/rosenheim/oop/SmallCity.java
@@ -1,4 +1,18 @@
 package th.rosenheim.oop;
 
 public class SmallCity extends City {
+
+    public SmallCity(String cityName, int weatherCondition) {
+        super(cityName, weatherCondition);
+    }
+
+    @Override
+    public String getContent() {
+        return "";
+    }
+
+    @Override
+    public String getURL() {
+        return "";
+    }
 }
