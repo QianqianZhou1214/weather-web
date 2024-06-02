0e9b3db3e847a54216844b204d1a7f2fdf6bfa7a
Qianqian Zhou <qianqian.zhou@stud.th-rosenheim.de>
2024-06-02 15:17:36 +0200

    Modify App implementation (main, init) accordingly

--- psta-src/src/main/java/th/rosenheim/oop/App.java
+++ psta-src/src/main/java/th/rosenheim/oop/App.java
@@ -12,16 +12,33 @@ public class App {
         init();
     }
 
-
-
-
     public static void init() {
+
+        //create objects of small and big cities and initialize the constructors
+        //initialize the big cities constructor with a new arraylist in order to add districts
+
+        SmallCity rosenheim = new SmallCity("Rosenheim", WeatherCondition.CLOUDY.ordinal());
+
+        BigCity munich = new BigCity("München", WeatherCondition.RAINY.ordinal(),
+                new ArrayList<>());
+        //add districts for big cities
+        munich.addDistricts("Schwabing");
+        munich.addDistricts("Sendling");
+        munich.addDistricts("Lehel");
+        munich.addDistricts("Nymphenburg");
+
+        //can also initialize the arraylist with districts names, if we have more to add later, we can use addDistricts() method
+        BigCity berlin = new BigCity("Berlin", WeatherCondition.SUNNY.ordinal(),
+                new ArrayList<>(List.of("Kreuzberg", "Charlottenburg", "Prenzlauer Berg", "Friedrichshain", "Neukölln", "Schöneberg")));
+
+        //create the object of WebsiteGenerator, the constructor automatically creates a new ArrayList for cities in the website
         WebsiteGenerator website = new WebsiteGenerator();
-        website.addCities(new SmallCity("Rosenheim", WeatherCondition.CLOUDY.ordinal()));
-        website.addCities(new BigCity("München", WeatherCondition.RAINY.ordinal(),
-                new ArrayList<>(List.of("Schwabing", "Sendling", "Lehel", "Nymphenburg"))));
-        website.addCities(new BigCity("Berlin", WeatherCondition.SUNNY.ordinal(),
-                new ArrayList<>(List.of("Kreuzberg", "Charlottenburg", "Prenzlauer Berg", "Friedrichshain", "Neukölln", "Schöneberg"))));
+        //add cities to website
+        website.addCities(rosenheim);
+        website.addCities(munich);
+        website.addCities(berlin);
+
+        berlin.removeDistrict("Kreuzberg");
 
         try{
             website.generateWebsites("The Weather App.html");
